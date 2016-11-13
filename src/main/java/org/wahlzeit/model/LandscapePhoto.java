3c79bb57052a30d80f5c42dcce1461d12017ba3c
Gabriel Graf <gabrielgraf93@gmail.com>
2016-11-13 17:05:57 +0100

    Registered LandscapePhoto in OfyService, added Subclass annotation to LandscapePhoto class

--- src/main/java/org/wahlzeit/model/LandscapePhoto.java
+++ src/main/java/org/wahlzeit/model/LandscapePhoto.java
@@ -1,8 +1,11 @@
 package org.wahlzeit.model;
 
+import com.googlecode.objectify.annotation.Subclass;
+
 /**
  * A landscape photo represents a user-provided (uploaded) photo.
  */
+@Subclass(index = true)
 public class LandscapePhoto extends Photo {
 
 	/**
